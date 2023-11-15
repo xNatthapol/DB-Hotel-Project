da556861cec767b6506887cda7c7992d6a3f9392
Natthadit LERTPISANWUT <natthadit.l@ku.th>
2023-11-15 22:20:41 +0700

    modify attribute in finan table

--- hotel/models/financial.py
+++ hotel/models/financial.py
@@ -1,18 +1,14 @@
 from django.db import models
 
 
+class Payment(models.Model):
+    payment_datetime = models.DateTimeField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+
 class Account(models.Model):
-    id_account = models.CharField(primary_key=True, max_length=10)
     owner_name = models.CharField(max_length=255)
     income = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     outcome = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-
-
-class Payment(models.Model):
-    transaction_id = models.CharField(primary_key=True, max_length=10)
-    payment_datetime = models.DateTimeField()
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    payer = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='payments_made')
-    receiver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='payments_received')
-
+    transaction_id = models.ForeignKey(Payment, on_delete=models.CASCADE)
