91b56af72e79e24af596cb80b5e7bb39d1332848
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-11-15 23:17:44 +0700

    Update Role and Employee in models

--- hotel/models/worker.py
+++ hotel/models/worker.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Role(models.Model):
+    role_name = models.CharField(max_length=255)
+    base_salary = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class Employee(models.Model):
-    class Meta:
-        abstract = True
-
-
-class HeadChef(Employee):
-    pass
-
-
-class Accountant(Employee):
-    pass
+    role = models.ForeignKeyField(Role, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    hire_date = models.DateField(default=timezone.now)
