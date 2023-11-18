0ed3735fc882567d40d534656e3bb63452f7cc15
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-11-18 13:05:53 +0700

    Add email to Employee

--- hotel/models/worker.py
+++ hotel/models/worker.py
@@ -12,4 +12,5 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     hire_date = models.DateField(default=timezone.now)
