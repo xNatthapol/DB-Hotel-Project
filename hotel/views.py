b0b8ba9183f4d03ffc8c8435d1872952c1ffecc7
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-19 22:57:27 +0700

    up

--- hotel/views.py
+++ hotel/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.apps import apps
 
 def export_table(request, app_name, model_name, fields=None):
+    # table = request.GET.get('search')
     model = apps.get_model(app_label=app_name, model_name=model_name)
     
     if fields is None:
-        fields = [field.name for field in model._meta.get_fields()]
+        fields = [field.name for field in model._meta.get_fields() if not field.is_relation]
 
     data = model.objects.values(*fields)  # Retrieve only the specified fields
     return render(request, 'export_table.html', {'data': data, 'fields': fields})
@@ -16,3 +17,12 @@ def redirect_to_export_table(request, *args, **kwargs):
     model_name = kwargs.get('model_name', 'Employee')
     
     return redirect('hotel:export_table', app_name=app_name, model_name=model_name)
+
+def your_view(request):
+    # Get all models in the app
+    models = apps.get_models()
+
+    # Extract model names
+    model_names = [model.__name__ for model in models]
+
+    return render(request, 'index.html', {'options': model_names})
