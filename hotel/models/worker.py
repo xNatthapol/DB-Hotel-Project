b61ef4b7407d4d4cf4033c57465f4098add25640
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-19 21:32:16 +0700

    up

--- hotel/models/worker.py
+++ hotel/models/worker.py
@@ -9,8 +9,8 @@ class Role(models.Model):
 
 class Employee(models.Model):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, null=True, blank=True)
-    last_name = models.CharField(max_length=50, null=True, blank=True)
-    phone_number = models.CharField(max_length=10, null=True, blank=True)
-    email = models.EmailField(max_length=100, null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
     hire_date = models.DateField(default=timezone.now)
