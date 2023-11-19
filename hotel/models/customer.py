13917defdf732b111b1b0a7bec780c7e3f859fb8
Chanun SUNTRAPIWAT <chanun.s@ku.th>
2023-11-19 21:15:14 +0700

    change max_length to reasonnable value

--- hotel/models/customer.py
+++ hotel/models/customer.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=10)
-    fname = models.CharField(max_length=50)
-    sname = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     checkin_time = models.DateTimeField()
     checkout_time = models.DateTimeField()
